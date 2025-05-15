aa6556d74aa74d69fe14af18bfa842ac34d794b7
nodara3 <nodiko28@gmail.com>
2025-05-15 22:09:13 +0200

    v,hsync FSM added

--- HDMI_Transmitter/HDMI_Transmitter.srcs/sources_1/new/hdmi_interface.v
+++ HDMI_Transmitter/HDMI_Transmitter.srcs/sources_1/new/hdmi_interface.v
@@ -20,58 +20,271 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module hdmi_interface(
+module hdmi_interface#(
+
+    parameter TMDS_Channel = 0,
+    parameter Horizontal_Visible_Pixels = 1440,
+    parameter Horizontal_Front_Porch = 80,
+    parameter Horizontal_Sync_Pulse = 152,   //(active-high)
+    parameter Horizontal_Back_Porch = 232,
+    parameter Vertical_Visible_Lines = 900,
+    parameter Vertical_Front_Porch = 1,
+    parameter Vertical_Sync_Pulse = 3,  //(active-high)
+    parameter Vertical_Back_Porch = 28
+    
+    )
+    (
     input clk,
     input resetn,
     
-    output wire hSync,
-    output wire vSync
+    output reg [1:0] control_data,
+    output reg [7:0] pixel_data,
+    output reg [3:0] aux_data,
+    output [2:0] h_state,
+    output [2:0] v_state
     );
     
-    reg cur_state[2:0];
-    reg next_state[2:0];
+    reg [2:0] h_cur_state, v_cur_state;
+    reg [2:0] h_next_state, v_next_state;
+    reg count_enable;
+    reg hStandby;
+    integer hCounter, vCounter;
     
-    parameter IDLE = 0, Control_Period = 1, Data_Island_Period = 2, Video_Data_Period = 3;
+    reg vSync_Pulse, hSync_Pulse;
+    reg [3:0] CTL;
+    localparam Vertical_Pulse_Period = Vertical_Front_Porch + Vertical_Sync_Pulse + Vertical_Back_Porch;
+    localparam Vertical_Full_Period = Vertical_Pulse_Period + Vertical_Visible_Lines;
+    
+    localparam Preamble_Size = 8;
+    localparam Video_Guard_Size = 2;
+  
+    localparam Horizontal_Pulse_Period = Horizontal_Front_Porch + Horizontal_Sync_Pulse + Horizontal_Back_Porch;
+    localparam Horizontal_Full_Period = Vertical_Pulse_Period + Horizontal_Visible_Pixels; 
+    
+    localparam IDLE = 0, hFront = 1, hSync = 2, hBack = 3, Preamble = 4, Video_Guard = 5, Video_Data_Period = 6;
+    localparam vFront = 1, vSync = 2, vBack = 3, vPixel = 4;
+    
+    assign v_state = v_cur_state;
+    assign h_state = h_cur_state;
+    
+    always @ (posedge clk) begin
+
+        if (! resetn) begin
+            h_cur_state <= IDLE;
+            v_cur_state <= IDLE;
+            hCounter <= 0;
+            vCounter <= 0;
+
+        end else begin
+        
+//            if (h_cur_state != h_next_state) begin
+//                hCounter <= 0;
+//            end else begin
+//                hCounter <= hCounter + 1;
+//            end
+            
+            
+            if (hCounter < Horizontal_Full_Period - 2) begin
+                hCounter <= hCounter + 1;
+                
+            end else if (hCounter < Horizontal_Full_Period - 1)begin
+            
+                hCounter <= hCounter + 1;
+                if (vCounter < Vertical_Full_Period - 1) begin
+                    vCounter <= vCounter + 1;
+                end else begin
+                    vCounter <= 0;
+                end
+             end else begin   
+                hCounter <= 0;
+                
+                //increment vCounter before it runs whole vertical 
+                
+            end
+            
+            // State Update 
+            h_cur_state <= h_next_state;
+            v_cur_state <= v_next_state;
+        end
+    end
+  
+    
     
     always @ (posedge clk) begin
         // If reset is asserted, go back to IDLE state
         if (! resetn) begin
-            cur_state <= IDLE;
 
         // Else transition to the next state
         end else begin
-            cur_state <= next_state;
+            case(TMDS_Channel)
+                0: begin
+                    control_data[0] = vSync_Pulse;
+                    control_data[1] = hSync_Pulse;
+                end
+                    
+                
+                1 : begin
+                    control_data[0] = CTL[0];
+                    control_data[1] = CTL[1];
+                end
+                
+                2 : begin
+                    control_data[0] = CTL[2];
+                    control_data[1] = CTL[3];                
+                end 
+                
+                default: ;
+            endcase
+        end
+    end  
+    
+
+    // Vertical Sync FSM
+    always @ (*) begin
+       if (!resetn) begin
+            v_next_state = IDLE;
+            vSync_Pulse = 0;
+       end else begin 
+            case(v_cur_state)
+                
+                IDLE: begin
+                    vSync_Pulse = 0;
+                    if (!resetn) begin
+                        v_next_state = IDLE;
+                    end else begin
+                        v_next_state = vFront;
+                    end
+                end
+                     
+                vFront : begin
+                    vSync_Pulse = 0;
+                    if (vCounter < Vertical_Front_Porch) begin
+                        v_next_state = vFront;
+                    end else begin
+                        v_next_state =  vSync;
+                    end    
+                end
+    
+                vSync : begin
+                    vSync_Pulse = 1;
+                    if (vCounter < Vertical_Front_Porch + Vertical_Sync_Pulse) begin
+                        v_next_state = vSync;
+                    end else begin
+                        v_next_state = vBack;
+                    end
+                end
+                
+                vBack  : begin
+                    vSync_Pulse = 0;
+                    if (vCounter < Vertical_Pulse_Period) begin
+                        v_next_state = vBack;
+                    end else begin
+                        v_next_state = vPixel;
+                    end
+                    
+                end
+                
+                vPixel : begin
+                    
+                    vSync_Pulse = 0;
+                
+                   if (vCounter < Vertical_Full_Period) begin
+                        v_next_state = vPixel;
+                    end else begin
+                        v_next_state = vFront ;
+                    end
+                end
+                
+                default: v_next_state = IDLE;
+            
+            endcase
         end
     end
-  
-  
     
+    // Horizontal Sync FSM
     always @ (*) begin
         
-        next_state = IDLE;
-        
-        case(cur_state)
-            IDLE: begin
+        if (!resetn) begin
+            h_next_state = IDLE;
+            hSync_Pulse = 0;
+        end else begin 
             
-            end
-            
-            
-            Control_Period : begin
-            
-            end
-            
-            Data_Island_Period : begin
-            
-            end
-            
-            Video_Data_Period : begin
-            
-            end
-            
-            default: next_state = IDLE;
-        
-        endcase
+            case(h_cur_state)
+                
+                IDLE: begin
+                    hSync_Pulse = 0;
+                    if (!resetn) begin
+                        h_next_state = IDLE;
+                    end else begin
+                        h_next_state = hFront;
+                    end
+                end
+                     
+                hFront : begin
+                    hSync_Pulse = 0;
+                    if (hCounter < Horizontal_Front_Porch - 1) begin
+                        h_next_state = hFront;
+                    end else begin
+                        h_next_state = hSync;
+                    end   
+                end
     
+                hSync : begin
+                    hSync_Pulse = 1;
+                    if (hCounter < Horizontal_Front_Porch + Horizontal_Sync_Pulse - 1) begin
+                        h_next_state = hSync;
+                    end else begin
+                        h_next_state = hBack;
+                    end
+                end
+                
+                hBack  : begin
+                    hSync_Pulse = 0;
+                    if (hCounter < Horizontal_Pulse_Period - Preamble_Size - Video_Guard_Size - 1) begin
+                        h_next_state = hBack;
+                    end else begin
+                        h_next_state = Preamble;
+                    end
+                    
+                end
+                
+                Preamble : begin
+                
+                   hSync_Pulse = 0;
+                   CTL = 4'b0001;
+                    
+                   if (hCounter < Horizontal_Pulse_Period - Video_Guard_Size - 1) begin
+                        h_next_state = Preamble;
+                    end else begin
+                        h_next_state = Video_Guard;
+                    end
+                end
+                
+                Video_Guard : begin
+                    hSync_Pulse = 0;
+                   if (hCounter < Horizontal_Pulse_Period - 1) begin
+                        h_next_state = Video_Guard;
+                    end else begin
+                        h_next_state = Video_Data_Period;
+                    end    
+                           
+                end 
+                
+                Video_Data_Period : begin
+                    hSync_Pulse = 0;
+                    if (hCounter < Horizontal_Full_Period - 1) begin
+                        h_next_state = Video_Data_Period;
+                    end else begin
+                        h_next_state = hFront ;
+                    end
+                    
+                end
+                
+                
+                default: h_next_state = IDLE;
+            
+            endcase
+        end
     end
     
 endmodule
