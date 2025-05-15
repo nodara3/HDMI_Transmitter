e0d0e3dfba2480965e225d7f1e41d2ea3044db51
nodara3 <nodiko28@gmail.com>
2025-05-15 22:09:53 +0200

    MMCM added for clock, still not complete

--- HDMI_Transmitter/HDMI_Transmitter.srcs/sources_1/new/hdmi_transmitter_top.v
+++ HDMI_Transmitter/HDMI_Transmitter.srcs/sources_1/new/hdmi_transmitter_top.v
@@ -26,7 +26,10 @@ module hdmi_transmitter_top(
     //Main Reset
         input rstn,
         
-        input [7:0] pixel_data
+        
+        output pixel_clk,
+        output m_clk
+//        input [7:0] pixel_data
       
 //    //HDMI Outputs
 //        output  wire  hdmi_tx0_p,
@@ -45,15 +48,103 @@ module hdmi_transmitter_top(
 //        output out2
     );
     
+    wire [1:0] control_data;
+    wire [7:0] pixel_data;
+    wire [3:0] aux_data;
+    wire [2:0] h_state;
+    wire [2:0] v_state;
     
-    encoder u_encode_blue(
+    encoder #(
+        .TMDS_Channel(0) 
+        
+    ) u_encode_blue (
         .clk(clk_100MHz),
         .resetn(rstn),
-        .pixel_data(pixel_data)
+        .pixel_data(pixel_data),
+        .control_data(control_data),
+        .aux_data(aux_data),
+        .h_state(h_state),
+        .v_state(v_state)
     );
     
     
+    hdmi_interface #(
 
+        .TMDS_Channel(0)
+    
+    ) u_hdmi_interface0 (
+        .clk(clk_100MHz),
+        .resetn(rstn),
+        .control_data(control_data),
+        .aux_data(aux_data),
+        .h_state(h_state),
+        .v_state(v_state)
+    );
+    
+    
+    //CLOCK
+    wire clkfb;
+    wire locked;
+    MMCME2_BASE #(
+        .BANDWIDTH("OPTIMIZED"), // Jitter programming (OPTIMIZED, HIGH, LOW)
+        .CLKFBOUT_MULT_F(10.647), // Multiply value for all CLKOUT (2.000-64.000).  // get 1.0647 GHz
+        .CLKFBOUT_PHASE(0.0), // Phase offset in degrees of CLKFB (-360.000-360.000).
+        .CLKIN1_PERIOD(10.0), // Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
+        // CLKOUT0_DIVIDE - CLKOUT6_DIVIDE: Divide amount for each CLKOUT (1-128)
+        .CLKOUT1_DIVIDE(10),
+        .CLKOUT2_DIVIDE(1),
+        .CLKOUT3_DIVIDE(1),
+        .CLKOUT4_DIVIDE(1),
+        .CLKOUT5_DIVIDE(1),
+        .CLKOUT6_DIVIDE(1),
+        .CLKOUT0_DIVIDE_F(1.0), // Divide amount for CLKOUT0 (1.000-128.000).
+        // CLKOUT0_DUTY_CYCLE - CLKOUT6_DUTY_CYCLE: Duty cycle for each CLKOUT (0.01-0.99).
+        .CLKOUT0_DUTY_CYCLE(0.5),
+        .CLKOUT1_DUTY_CYCLE(0.5),
+        .CLKOUT2_DUTY_CYCLE(0.5),
+        .CLKOUT3_DUTY_CYCLE(0.5),
+        .CLKOUT4_DUTY_CYCLE(0.5),
+        .CLKOUT5_DUTY_CYCLE(0.5),
+        .CLKOUT6_DUTY_CYCLE(0.5),
+        // CLKOUT0_PHASE - CLKOUT6_PHASE: Phase offset for each CLKOUT (-360.000-360.000).
+        .CLKOUT0_PHASE(0.0),
+        .CLKOUT1_PHASE(0.0),
+        .CLKOUT2_PHASE(0.0),
+        .CLKOUT3_PHASE(0.0),
+        .CLKOUT4_PHASE(0.0),
+        .CLKOUT5_PHASE(0.0),
+        .CLKOUT6_PHASE(0.0),
+        .CLKOUT4_CASCADE("FALSE"), // Cascade CLKOUT4 counter with CLKOUT6 (FALSE, TRUE)
+        .DIVCLK_DIVIDE(1), // Master division value (1-106)
+        .REF_JITTER1(0.0), // Reference input jitter in UI (0.000-0.999).
+        .STARTUP_WAIT("FALSE") // Delays DONE until MMCM is locked (FALSE, TRUE)
+        )
+        MMCME2_BASE_inst (
+        // Clock Outputs: 1-bit (each) output: User configurable clock outputs
+        .CLKOUT0    (pixel_clk), // 1-bit output: CLKOUT0 // TMDS Pixel Clock
+        .CLKOUT0B   (), // 1-bit output: Inverted CLKOUT0 
+        .CLKOUT1    (m_clk), // 1-bit output: CLKOUT1    // Normal CLock
+        .CLKOUT1B   (), // 1-bit output: Inverted CLKOUT1
+        .CLKOUT2    (), // 1-bit output: CLKOUT2
+        .CLKOUT2B   (), // 1-bit output: Inverted CLKOUT2
+        .CLKOUT3    (), // 1-bit output: CLKOUT3
+        .CLKOUT3B   (), // 1-bit output: Inverted CLKOUT3
+        .CLKOUT4    (), // 1-bit output: CLKOUT4
+        .CLKOUT5    (), // 1-bit output: CLKOUT5
+        .CLKOUT6    (), // 1-bit output: CLKOUT6
+        // Feedback Clocks: 1-bit (each) output: Clock feedback ports
+        .CLKFBOUT   (clkfb), // 1-bit output: Feedback clock
+        .CLKFBOUTB  (), // 1-bit output: Inverted CLKFBOUT
+        // Status Ports: 1-bit (each) output: MMCM status ports
+        .LOCKED(locked), // 1-bit output: LOCK
+        // Clock Inputs: 1-bit (each) input: Clock input
+        .CLKIN1     (clk_100MHz), // 1-bit input: Clock
+        // Control Ports: 1-bit (each) input: MMCM control ports
+        .PWRDWN     (), // 1-bit input: Power-down
+        .RST        (resetn), // 1-bit input: Reset
+        // Feedback Clocks: 1-bit (each) input: Clock feedback ports
+        .CLKFBIN    (clkfb) // 1-bit input: Feedback clock
+        );
 
     
     
